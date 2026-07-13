c00d4f59b50b96c629c578b382560d7a9719d7aa
Jesus Valdes <jesus_vc2205@hotmail.com>
2026-07-13 14:27:59 -0600

    Se quitan animaciones

--- js/home.js
+++ js/home.js
@@ -1,16 +1,5 @@
 function initHome() {
 
-    //console.log("Home cargado");
-    
-    observeElement(".home-section", () => {
-        fadeUp(".home-title", 100);
-        fadeUp(".home-subtitle", 250);
-        fadeUp(".home-description", 400);
-        scaleIn(".home-quote", 550);
-        fadeUp(".home-buttons", 700);
-        fadeUp(".home-technologies-title", 850);
-        staggerUp(".technology-item");
-        fadeRight(".profile-image", 300);
-    });
+    console.log("Home cargado");
 
 }
